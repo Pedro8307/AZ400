cbfa55bfb338dd0b48a449224b85bc8e3f54c4cd
Pedro <49094202+Pedro8307@users.noreply.github.com>
2024-12-03 14:13:50 +0000

    V9

--- List AZ-ResourceGroups.py
+++ List AZ-ResourceGroups.py
@@ -1,13 +1,18 @@
-import os
 from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
 
+# Authenticate using AzureCliCredential
 credential = AzureCliCredential()
-subscription_id = os.environ["a2370bc5-c828-41f1-8c20-0d4f829f108d"]
 
+# Your Azure subscription ID
+subscription_id = "a2370bc5-c828-41f1-8c20-0d4f829f108d"
+
+# Initialize Resource Management Client
 resource_client = ResourceManagementClient(credential, subscription_id)
 
+# List resource groups
+print("Listing resource groups in subscription:")
 rg_list = resource_client.resource_groups.list()
 
 for rg in rg_list:
-    print(rg.name)
+    print(f"- {rg.name}")
