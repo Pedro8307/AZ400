90cb9250129bbf3574f40cc8e400dc187b8b7910
Pedro <49094202+Pedro8307@users.noreply.github.com>
2024-12-03 11:20:00 +0000

    V5

--- AZ-ResourceGroup.py
+++ AZ-ResourceGroup.py
@@ -9,14 +9,14 @@ os.environ["AZURE_CLI_PATH"] = "/usr/bin/az"  # Adjust if your 'which az' comman
 credential = AzureCliCredential()
 
 # Use your Azure subscription ID
-subscription_id = "a2370bc5-c828-41f1-8c20-0d4f829f108d"
+subscription_id = "VALUE"
 
 # Initialize Resource Management Client
 resource_client = ResourceManagementClient(credential, subscription_id)
 
 # Define the resource group parameters
-resource_group_name = "Az-400"  # Resource group name
-location = "uksouth"           # Azure region
+resource_group_name = "VALUE"  # Resource group name
+location = "VALUE"           # Azure region
 
 # Create or update the resource group
 print(f"Creating resource group '{resource_group_name}' in location '{location}'...")
